5e85ffcd03262dd4cdb6ed8a94ed9281dd3129c3
PotatoArch <alhassanoday.ffxiv@gmail.com>
2025-06-26 21:28:37 +0300

    tmux bug fix

--- fish/conf.d/tmux.fish
+++ fish/conf.d/tmux.fish
@@ -1,5 +1,8 @@
 if not set -q TMUX
-    set -g TMUX tmux new-session -d -s base
-    eval $TMUX
-    tmux attach-session -d -t base
+    set session_name base
+    if tmux has-session -t $session_name 2>/dev/null
+        exec tmux new-window -t $session_name \; attach-session -t $session_name
+    else
+        exec tmux new-session -s $session_name
+    end
 end
